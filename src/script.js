e2ab0df6fe7229ee09ed62d8412f083e5594b61b
MsLVictor <victormleite25@icloud.com>
2023-05-14 09:57:45 -0300

    add botão de cores frias

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function generateColor() {
-
+    //cria cor aleatória
     const letters = '0123456789ABCDEF'
 
     let color = '#';
@@ -23,3 +23,26 @@ document.getElementById('btn-action').addEventListener('click', () => {
     }
 
 })
+
+function getRandomCoolColor() {
+    // Define uma cor fria aleatória com tons de azul e verde
+    var red = 0;
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    
+    // Converte os valores das componentes RGB em um valor hexadecimal
+    var hexRed = red.toString(16).padStart(2, '0');
+    var hexGreen = green.toString(16).padStart(2, '0');
+    var hexBlue = blue.toString(16).padStart(2, '0');
+    
+    // Retorna a cor em formato hexadecimal
+    return "#" + hexRed + hexGreen + hexBlue;
+  }
+
+  document.getElementById('btn-cfrio').addEventListener('click', () => {
+    for(let i = 0; i < 5; i++) {
+        let color = getRandomCoolColor()
+        let div_color = document.getElementById(`color-${i}`)
+        div_color.style.backgroundColor = getRandomCoolColor()
+    }
+  })
